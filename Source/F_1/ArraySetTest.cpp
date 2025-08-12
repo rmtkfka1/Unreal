5fa1446c1d807e2fbeb33b9f5f8bed68635442e4
kim sang hyeok <pakheae@nate.com>
2025-08-13 06:00:43 +0900

    어레이셋트

---
+++ Source/F_1/ArraySetTest.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ArraySetTest.h"
+#include "Algo/Accumulate.h"
+void UArraySetTest::Init()
+{
+	Super::Init();
+
+	{
+		const int32 ArrayNum = 10;
+
+		TArray<int32> v;
+		TArray<int32> v2;
+
+		for (int32 i = 1; i <= ArrayNum; ++i)
+		{
+			v.Emplace(i);
+		}
+
+
+		v.RemoveAll([](int32 val) {return val % 2 == 0; });
+		v += {2, 4, 6, 8, 10};
+
+		int32 temp[] = { 1,3,5,7,9,2,4,6,8,10 };
+		v2.AddUninitialized(ArrayNum);
+		FMemory::Memcpy(v2.GetData(), temp, sizeof(int32) * ArrayNum);
+		int32 sum = Algo::Accumulate(v, 0);
+
+		ensure(v == v2);
+	}
+
+	{
+		TSet<int32> s;
+
+		for (int32 i = 1; i <= 10; ++i)
+		{
+			s.Emplace(i);
+		}
+
+		s.Remove(2);
+		s.Remove(4);
+		s.Remove(6);
+		s.Remove(8);
+		s.Remove(10);
+
+		s.Emplace(2);
+		s.Emplace(4);
+		s.Emplace(6);
+		s.Emplace(8);
+		s.Emplace(10);
+
+		int32 a = 5;
+
+	}
+};
---
+++ Source/F_1/ArraySetTest.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ArraySetTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class F_1_API UArraySetTest : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Init() override;
+};
